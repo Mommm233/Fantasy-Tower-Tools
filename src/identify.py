6ea5b393baf7510cc39e36f298b8c25d7a278fc9
Mommm233 <88660188+Mommm233@users.noreply.github.com>
2024-04-27 06:39:03 +0800

    Add files via upload

--- src/identify.py
+++ src/identify.py
@@ -5,12 +5,12 @@ from pyautogui import screenshot
 from pygetwindow import pyrect
 
 class Identify:
-    def __init__(self, screen_img:np.ndarray) -> None:
+    def __init__(self, screen_img: np.ndarray) -> None:
         self.screen_img = screen_img
         self.height, self.width, _ = screen_img.shape
 
     # 判断part是否出现在img里
-    def part_is_in_img(self, img:np.ndarray, part:np.ndarray, threshold:int) -> bool:
+    def part_is_in_img(self, img: np.ndarray, part: np.ndarray, threshold: int) -> bool:
         if img is None or part is None:
             return False
         if img.size == 0 or part.size == 0:
@@ -24,20 +24,20 @@ class Identify:
         else:
             return False
 
-    def check(self, rect:list, sign:np.ndarray, threshold=0.8) -> bool:
+    def check(self, rect: list, sign: np.ndarray, threshold=0.8) -> bool:
         [x, y, w, h] = rect
         img = self.screen_img[y:y+h, x:x+w]
         # cv2.imwrite("img.png", img)
         # cv2.imwrite("sign.png", sign)
         return self.part_is_in_img(img, sign, threshold)
 
-    def get_screen_img(self, rect:pyrect.Rect) -> np.ndarray:
+    def get_screen_img(self, rect: pyrect.Rect) -> np.ndarray:
         screen_img = screenshot(region=(rect.left, rect.top, rect.width, rect.height))
         screen_img = cv2.cvtColor(np.asarray(screen_img), cv2.COLOR_RGB2BGR)
         return screen_img
     
     # 更新屏幕图像
-    def update(self, screen_img:np.ndarray) -> None:
+    def update(self, screen_img: np.ndarray) -> None:
         self.screen_img = screen_img
         self.height, self.width, _ = screen_img.shape
 
